38e1f89f3e2f30140ef52607e96853ecd91d6d23
aubertMal <malek.aubert@gmail.com>
2021-07-22 13:44:00 +0200

    feat: club interface

---
+++ src/test/java/com/aubert/tennisClub/TennisClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aubert.tennisClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
