6c47df98a3bdb2fc7f415b5b5f0a9891622b48f5
aubertMal <malek.aubert@gmail.com>
2021-07-23 11:43:31 +0200

    feat: display environement on homePage

--- src/main/java/com/aubert/tennisClub/controller/ViewController.java
+++ src/main/java/com/aubert/tennisClub/controller/ViewController.java
@@ -1,14 +1,20 @@
 package com.aubert.tennisClub.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class ViewController {
+    @Value("${displayed-name}")
+    private String displayedName;
+
     @GetMapping("clubInterface")
-    public String clubInterface(){
+    public String clubInterface(Model model){
+        model.addAttribute("applicationName",displayedName);
         return "clubInterface";
     }
 }
